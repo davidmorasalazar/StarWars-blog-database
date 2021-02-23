e02f283aeb14fbd605145ee026e56560d29089bb
davidmorasalazar <davidmsalazar99@gmail.com>
2021-02-23 18:54:42 +0000

    my commit 1

--- src/models.py
+++ src/models.py
@@ -8,26 +8,40 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class Favorites(Base): 
+    __tablename__ = 'Favorites'
+    User_ID = Column(Integer,primary_key=True)
+    # FavoritesPlanet_ID = Column(Integer)
+    # FavoritesPerson_ID = Column(Integer)
+
+
+class CardPerson(Base):
+    __tablename__ = 'CardPerson'
+    #Siempre usar guion bajo cuando se hace una tabla
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-
-class Address(Base):
-    __tablename__ = 'address'
+    Person_name = Column(String(250), nullable=False)
+    Hair_color = Column(String(250))
+    Skin_color = Column(String(250))
+    Gender  = Column(String(250))
+    Birth_year = Column(Integer, nullable=False)
+    height = Column(Integer)
+class CardPlanet(Base):
+    __tablename__ = 'CardPlanet'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    Planet_name = Column(String(250), nullable=False)
+    Population = Column(Integer)
+    Terrain = Column(String(250))
+    Climate = Column(String(250))
+    Orbital_Period = Column(Integer)
+    #person_id = Column(Integer, ForeignKey('CardPerson.id'))
+    person = relationship(CardPerson)
 
     def to_dict(self):
         return {}
 
 ## Draw from SQLAlchemy base
-render_er(Base, 'diagram.png')
+render_er(Base, 'diagram2.png')
